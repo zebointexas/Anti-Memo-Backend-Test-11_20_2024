5c7c311d5893d84d4e411532782e70442413a0f5
The Bear @ Austin <46735920+zebointexas@users.noreply.github.com>
2024-11-24 22:43:00 -0800

    asd

--- backend_Demo_Test/settings.py
+++ backend_Demo_Test/settings.py
@@ -105,19 +105,13 @@ WSGI_APPLICATION = 'backend_Demo_Test.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join('/tmp', 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
